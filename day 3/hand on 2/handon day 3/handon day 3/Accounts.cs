e54bdbe349cc173f02e6a26eef5300c4f951eabd
vajahullasharif <vajahullamohmmed@gmail.com>
2022-04-21 16:19:49 +0530

    handonday3

---
+++ day 3/hand on 2/handon day 3/handon day 3/Accounts.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handon_day_3
+{
+    internal class Accounts
+    {
+        
+
+        int Accountno;
+        string Customerno;
+        string AccountType;
+        float amount = 1000f; public void credit(float amt)
+        {
+            amount += amt;
+            Console.WriteLine(amt + " Deposited");
+            Console.WriteLine("Balance is " + amount);
+        }
+        public void debit(float amt)
+        {
+            if (amount < amt)
+            {
+                Console.WriteLine("Insufficent balance");
+            }
+            else
+            {
+                amount -= amt;
+                Console.WriteLine(amt + " with drawn");
+                Console.WriteLine("Balance is" + amount);
+            }
+        }
+        public Accounts()
+        {
+            Console.WriteLine("Enter Accountno");
+            Accountno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter customerno ");
+            Customerno = Console.ReadLine();
+            Console.WriteLine("Enter Account type");
+            AccountType = Console.ReadLine();
+        }
+        public void showdetails()
+        {
+            Console.WriteLine("account no is " + Accountno + " customer no is " + Customerno + " account type " + AccountType + " balance is " + amount);
+        }
+
+
+
+    }
+}
---
+++ day 3/hand on 2/handon day 3/handon day 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace handon_day_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Accounts accounts = new Accounts();
+
+            accounts.credit(4000.00f);
+            //accounts.debit(3000.00f);
+            accounts.showdetails();
+            Console.ReadLine();
+        }
+    }
+}
+       
